20fbd3361408cbab5cf9f70c073723ee28b59c1c
Faiznation <novaatalagrab@gmail.com>
2024-12-08 22:52:52 +0700

    fix spotify

--- js/music-list.js
+++ js/music-list.js
@@ -1,89 +1,13 @@
+// Example to populate playlist dynamically
+const playlistContainer = document.getElementById('playlist');
 
-let allMusic = [
-
-    {
-  
-      name: "Beautiful",
-  
-      artist: "Akon",
-  
-      img: "Music/kvt_cheche_akon-beautiful-remix-reggae.mp3",
-  
-      src: "Music/kvt_cheche_akon-beautiful-remix-reggae.mp3"
-  
-    },
-    
-    {
-  
-      name: "Old Love",
-  
-      artist: "yuji,putri dahlia",
-  
-      img: "music-1",
-  
-      src: "music-1"
-  
-    },
-  
-    {
-  
-      name: "Eyes Blue X Terpikat Senyummu",
-  
-      artist: "Stromxx",
-  
-      img: "music-2",
-  
-      src: "music-2"
-  
-    },
-  
-    {
-  
-      name: "Losing Us",
-  
-      artist: "Raissa Anggiani",
-  
-      img: "music-3",
-  
-      src: "music-3"
-  
-    },
-  
-    {
-  
-      name: "Locked out of Heaven",
-  
-      artist: "Bruno Mars",
-  
-      img: "music-4",
-  
-      src: "music-4"
-  
-    },
-  
-    {
-  
-      name: "Love Story",
-  
-      artist: "Taylor Swift",
-  
-      img: "music-5",
-  
-      src: "music-5"
-  
-    },
-  
-    {
-  
-      name: "Menemukanmu",
-  
-      artist: "Seventeen",
-  
-      img: "music-6",
-  
-      src: "music-6"
-  
-    },
-  
- 
-  ];
+playlist.forEach((song, index) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = `${song.name} - ${song.artist}`;
+  listItem.addEventListener('click', () => {
+    currentSongIndex = index;
+    loadSong(song);
+    audio.play();
+  });
+  playlistContainer.appendChild(listItem);
+});
